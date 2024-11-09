558428ec7e7d14c2f9431f0fae771aa285462df9
Giulia Maineri <maineri.giulia@gmail.com>
2024-11-09 09:46:29 +0100

    bhi

--- searchbar.js
+++ searchbar.js
@@ -1,21 +1,21 @@
 function filterSongs() {
-    // Ottieni il valore della barra di ricerca e convertilo in minuscolo
+    // Ottieni il valore della barra di ricerca in minuscolo
     const searchTerm = document.getElementById("searchBar").value.toLowerCase();
-
     // Ottieni tutti gli elementi delle canzoni
-    const songs = document.querySelectorAll(" .square");
+    const songs = document.querySelectorAll(".square");
+    let found = false;
 
-    // Itera tra le canzoni e mostra/nascondi in base alla ricerca
+    // Itera sui blocchi delle canzoni
     songs.forEach(song => {
-        const title = song.querySelector("square-title").textContent.toLowerCase();
-
-        // Se il titolo contiene il termine di ricerca, mostra la canzone; altrimenti, nascondila
+        const title = song.querySelector(".square-title").textContent.toLowerCase();
         if (title.includes(searchTerm)) {
             song.style.display = "block";
+            found = true;
         } else {
             song.style.display = "none";
         }
     });
+    
     const message = document.getElementById("searchMessage");
     if (found && searchTerm !== "") {
         message.textContent = "Trovata!";
